bd4954c80fc4ccd20bb0489f2123039ee70015fa
livv8 <oliviahib898@gmail.com>
2021-10-24 21:39:15 -0500

    Update

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 /* Add your JavaScript to this file */
+/*var mail= getElementsById('email');
+
+function email(){
+    if(document.getElementsByClassName('btn').innerhtml)
+            document.getElementsByClassName('message').innerhtml= "Thank you! Your email address"+ mail + " has been added to our mailing list!";
+        else
+        document.getElementsByClassName('message').innerhtml= "Thank you! Your email address has been added to our mailing list!";
+
+}*/
+
+function mail(){
+    alert("Thank you! Your email address"+ mail + " has been added to our mailing list!");
+}
+document.getElementsByClassName("btn").onclick= mail;
